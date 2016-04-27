b0db11be8a55ac52ece5ff917a7361cd8ff1b171
Poroing <jean-18-09@hotmail.fr>
2016-04-27 19:15:10 +0200

    Commented graph.ml

--- Caml/graph.ml
+++ Caml/graph.ml
@@ -5,6 +5,10 @@ TODO: Implémenter une arbre binaire pour enlever la recherche linéaire
 à la ligne 9
 *)
 let is_con next is_end snode =
+    (* Parcours si on peut atteindre une node vérifiant `is_end` en
+        partant de `snode` en ne passant que par les node connecté entre
+        elle. les node connecté à une node étant donné par la fonction
+        `next` *)
     let rec aux next is_end gone snode =
         (not is_in gone snode) 
         && ((is_end snode) || (any (aux next is_end (snode::gone)) (next snode)))
