6205ccd9fc20768250175c76d435345b4e5c4e0f
Poroing <jean-18-09@hotmail.fr>
2016-04-27 19:30:33 +0200

    Orthographe correction player.ml

--- Caml/player.ml
+++ Caml/player.ml
@@ -4,20 +4,20 @@
 #open "lstutil";;
 #open "graph";;
 
-(* on dit que deux cases sont connecté si elle sont reliée par une
+(* On dit que deux cases sont connectées si elles sont reliées par une
     suite de case de même couleur*)
 let color_win board color =
-    (* Renvoie si `color` à gagner sur `board` *)
+    (* Indique si `color` a gagné sur `board` *)
     let is_end_color = is_end_tile board color in
     (* fonction qui indique si une case est une case de fin *)
     let is_con_to_end_tile = (is_con (color_ngh_tiles board) is_end_color) in
-    (* fonction indique si une case est connecté à une case de fin *)
+    (* fonction indique si une case est connectée à une case de fin *)
     let starts = filter (is_color board color) (start_tiles board color) in
     any is_con_to_end_tile starts
 ;;
 
 let winner board = find_if (color_win board) Empty [Red; Blue];;
-    (* Indique qui à gagner sur le plateau `board`, si personne n'as gagner
+    (* Indique qui a gagné sur le plateau `board`, si personne n'a gagnée
         renvoie `Empty`*)
 
 let rec good_pos_for board will_play=
@@ -30,7 +30,7 @@ let rec good_pos_for board will_play=
         | _ -> other_color will_play
              
 and good_play board color tile =
-    (* Renvoie si le la case `tile` est un bon coups pour le joueur
+    (* Renvoie si le la case `tile` est un bon coup pour le joueur
         `color` sur le plateau `board` *)
     let next_board = set_tile_color board color tile in
     let next_player = other_color color in
@@ -40,7 +40,7 @@ and good_play board color tile =
 ;;
         
 let what_play board color =
-    (* Renvoie le coups à jouer sur le plateau `board` pour le joueur
+    (* Renvoie le coup à jouer sur le plateau `board` pour le joueur
         `color` *)
     let empty_tile = tile_of_color board Empty in
     find_if (good_play board color) (hd empty_tile) (tile_of_color board Empty)
