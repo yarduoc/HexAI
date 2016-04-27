6eec31a8e971a28a85fb1509c6ae92b0814f552b
Poroing <jean-18-09@hotmail.fr>
2016-04-27 19:09:08 +0200

    Commend funutil.ml

--- Caml/funutil.ml
+++ Caml/funutil.ml
@@ -1,6 +1,7 @@
 let fnot1 f a = (not f a);;
 
 let rec all_pred preds a =
+    (* renvoie si `a` vérifie tout la liste des  prédicats `preds` *)
     match preds with
         | [] -> true
         | pred::rpreds -> (pred a) && (all_pred rpreds a)
