b28f07644299ffb293e1406a90d35ec0b1ed063f
Poroing <jean-18-09@hotmail.fr>
2016-05-07 10:38:37 +0200

    Added better names for player.ml

--- Caml/player.ml
+++ Caml/player.ml
@@ -6,88 +6,88 @@
 
 (* On dit que deux cases sont connectées si elles sont reliées par une
     suite de case de même couleur*)
-let color_win board color =
+let winOnBoard board color =
     (* Indique si `color` a gagné sur `board` *)
-    let is_end_color = is_end_tile board color in
+    let isNextToEndSide = is_end_tile board color in
     (* fonction qui indique si une case est une case de fin *)
-    let is_con_to_end_tile = (is_con (color_ngh_tiles board) is_end_color) in
+    let isConnectedToEndSide = (isConnectedToEnd (color_ngh_tiles board) isNextToEndSide) in
     (* fonction indique si une case est connectée à une case de fin *)
-    let starts = filter (is_color board color) (start_tiles board color) in
-    any is_con_to_end_tile starts
+    let filled_start_tiles = filter (isColor board color) (getColorStartTiles board color) in
+    any isConnectedToEndSide filled_start_tiles
 ;;
 
-let winner board = find_if (color_win board) Empty [Red; Blue];;
+let winner board = findIf (winOnBoard board) Empty [Red; Blue];;
     (* Indique qui a gagné sur le plateau `board`, si personne n'a gagnée
         renvoie `Empty`*)
 
-let rec good_pos_for board will_play=
+let rec isGoodBoardFor board will_play=
     (* Indique si une position du plateau `board` est bonne pour le joueur
         `will_play` qui va jouer *)
-    let empty_tile = tile_of_color board Empty in
-    match empty_tile with
+    let empty_tiles = getTilesOfColor board Empty in
+    match empty_tiles with
         | [] -> winner board
-        | lst when any (good_play board will_play) lst -> will_play
-        | _ -> other_color will_play
+        | lst when any (isWinningPlay board will_play) lst -> will_play
+        | _ -> getOtherColor will_play
              
-and good_play board color tile =
+and isWinningPlay board color tile =
     (* Renvoie si le la case `tile` est un bon coup pour le joueur
         `color` sur le plateau `board` *)
-    let next_board = set_tile_color board color tile in
-    let next_player = other_color color in
-    let is_good = (good_pos_for next_board next_player) = color in
-    set_tile_color board Empty tile;
-    is_good
+    let board_after_play = set_tile_color board color tile in
+    let next_player = getOtherColor color in
+    let is_winning_play = (isGoodBoardFor next_board next_player) = color in
+    setTileColor board Empty tile;
+    is_winning_play
 ;;
         
-let what_play board color =
+let getWinningPlay board color =
     (* Renvoie le coup à jouer sur le plateau `board` pour le joueur
         `color` *)
-    let empty_tile = tile_of_color board Empty in
-    find_if (good_play board color) (hd empty_tile) empty_tile
-;;
-
-let add_pos_value will_play v1 v2 =
-    match v1, v2 with
-        | (r1, b1, w1), (r2, b2, w2) when w1 <> w2 ->
-            (r1 + r2, b1 + b2, will_play)
-        | (r1, b1, _), (r2, b2, _) -> 
-            (r1 + r2, b1 + b2, other_color will_play)
+    let empty_tiles = getTilesOfColor board Empty in
+    find_if (isWinningPlay board color) (hd empty_tiles) empty_tiles
 ;;
 
 (* La valeur d'un plateau est `(r, b, w)` ou `r` est le nombre de
     façon que `r` peut gagner en partant de ce plateau `b` est la
     même mais pour bleu et `w` la personne qui gagne si elle jout
     parfaitement en partant du plateau *)
+
+let addBoardsValue has_played v1 v2 =
+    match v1, v2 with
+        | (r1, b1, w1), (r2, b2, w2) when w1 <> w2 ->
+            (r1 + r2, b1 + b2, has_played)
+        | (r1, b1, _), (r2, b2, _) -> 
+            (r1 + r2, b1 + b2, getOtherColor has_played)
+;;
     
-let rec value_pos board will_play=
+let rec getBoardValue board will_play=
     (*Renvoie la valeur du plateau `board` sachant que le joueur
         `will_play` va jouer*)
-    let empty_tile = tile_of_color board Empty in
-    match empty_tile with
+    let empty_tiles = getTilesOfColor board Empty in
+    match empty_tiles with
         | [] -> 
-            let win = winner board in
+            let winner_of_board = winner board in
             begin
-                match win with
+                match winner_of_board with
                     | Blue -> (0, 1, Blue)
                     | Red -> (1, 0, Red)
-                    | Empty -> raise (Failure "prout")
+                    | Empty -> raise (Failure "There should at least be one winner")
             end
         | lst -> 
-            let sub_values = map (value_play board will_play) empty_tile in
+            let after_play_values = map (getBoardValueAfterPlay board will_play) empty_tile in
             (* liste des valeurs des plateaux ateignable depuis le
                 plateau `board` *)
-            list_it (add_pos_value will_play) (0, 0, other_color will_play) sub_values
+            reduce (addBoardsValue will_play) (0, 0, getOtherColor will_play) after_play_values
              
-and value_play board color tile =
+and getBoardValueAfterPlay board playing tile =
     (* Renvoie la valeur du tableau obtenue en faisant joué `color`
         sur la case `tile` sur le plateau `board` *)
-    set_tile_color board color tile;
-    let next_board_value = (value_pos board (other_color color)) in
-    set_tile_color board Empty tile;
+    setTileColor board playing tile;
+    let after_play_value = (getBoardValue board (getOtherColor playing)) in
+    setTileColor board Empty tile;
     next_board_value
 ;;
 
-let is_best_value color v1 v2 =
+let isBetterFor color v1 v2 =
     match v1, v2 with
         | ((r1, b1, w1), _), ((r2, b2, w2), _) ->
             w1 = color && w2 <> color
@@ -95,9 +95,9 @@ let is_best_value color v1 v2 =
             || color = Red && r1 > r2
 ;;
 
-let best_play board color =
+let getBestPlay board color =
     (* Renvoie le coup à jouer sur le plateau `board` pour le joueur
         `color` *)
-    let empty_tile = tile_of_color board Empty in
-    snd (max (is_best_value color) (im_and_ant (value_play board color) empty_tile))
+    let empty_tiles = getTilesOfColor board Empty in
+    snd (max (isBetterFor color) (getImagesAndAntecedants (getBoardValueAfterPlay board color) empty_tiles))
 ;;
